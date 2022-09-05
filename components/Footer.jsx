e4bd972259ab433a8589b8c1889c768ab35785b0
Juan José Arboleda <soyjuanarbol@gmail.com>
2022-09-04 23:36:33 -0500

    Redesign the webpage for my new me :)
    Signed-off-by: Juan José Arboleda <soyjuanarbol@gmail.com>

--- components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Footer extends Component {
-  render () {
-    return (
-      <span className='copyright'>Made with 💛 by <a href='https://twitter.com/soyjuanarbol'>@soyjuanarbol</a></span>
-    )
-  }
-}
--- entry.js
+++
@@ -1,5 +0,0 @@
-require('./components/MainComponent.jsx')
-require('./style/style.scss')
-require('./assets/img/unknowpleasures.jpeg')
-require('./assets/cv.pdf')
-require('./assets/favicon.ico')
